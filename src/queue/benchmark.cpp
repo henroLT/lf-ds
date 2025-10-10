5e9c020c24e6ed1e97a6c1e465bd971d29298ffc
Your Name <zhao.henry14@gmail.com>
2025-10-10 00:04:15 -0500

    working tester, further verification needed

--- src/queue/benchmark.cpp
+++ src/queue/benchmark.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <atomic>
 #include <cstdlib>
 #include "headers/lfqueue.hpp"
 
@@ -8,24 +9,20 @@ int main(int argc, char** argv) {
     using T = int;
 
     int numElements = 100;
-    int numThreads = 2;
+    int numThreads  = 4;
     if (argc > 1) numElements = std::atoi(argv[1]);
-    if (argc > 2) numThreads = std::atoi(argv[2]);
+    if (argc > 2) numThreads  = std::atoi(argv[2]);
     if (numElements < 0) numElements = 100;
     if (numThreads < 1) numThreads = 1;
 
     lfqueue<T> q;
-
-    std::cout << "Pushing " << numElements << " elements with "
-              << numThreads << " threads...\n";
-
     int perThread = (numElements + numThreads - 1) / numThreads;
 
     std::vector<std::thread> pushThreads;
     for (int t = 0; t < numThreads; ++t) {
-        pushThreads.emplace_back([t, perThread, &q]() {
+        pushThreads.emplace_back([t, perThread, &q, numElements]() {
             int start = t * perThread;
-            int end = start + perThread;
+            int end = std::min(start + perThread, numElements);
             for (int i = start; i < end; ++i) {
                 q.push(static_cast<T>(i));
             }
@@ -34,20 +31,16 @@ int main(int argc, char** argv) {
 
     for (auto& th : pushThreads) th.join();
 
-    std::cout << "All elements pushed.\n";
-
-
     std::vector<std::thread> popThreads;
     std::atomic<int> poppedCount{0};
 
     for (int t = 0; t < numThreads; ++t) {
         popThreads.emplace_back([&q, &poppedCount, numElements]() {
-            while (true) {
+            while (poppedCount < numElements) {
                 try {
                     T val = q.pop();
                     int count = ++poppedCount;
                     if (count % 10 == 0) std::cout << "Popped " << count << "\n";
-                    if (count >= numElements) break;
                 } catch (const std::runtime_error&) {
                     std::this_thread::yield();
                 }
